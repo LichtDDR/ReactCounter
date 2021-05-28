6721e876bfde8489ece4efb6b27ce497314f475f
Mikael Scientia <mikaelmuniz2001@gmail.com>
2021-05-28 17:54:50 -0300

    initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Counter = () => {
+    const [count, setCount] = React.useState(0);
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    }
+    
+    const handleDecrement = () => {
+        setCount(count - 1);
+    }
+    return(
+        <div>
+            <p>The counter is {count}</p>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
+        </div>
+    );
+};
+
+export default Counter;
