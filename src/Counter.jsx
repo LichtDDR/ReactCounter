3ac029d933db91b6a9a80dfbdf924468553a6c10
Mikael Scientia <mikaelmuniz2001@gmail.com>
2021-06-12 20:43:26 -0300

    updated!

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useEffect} from 'react';
 
 const Counter = () => {
@@ -25,32 +24,4 @@ const Counter = () => {
     );
 };
 
-=======
-import React, {useEffect} from 'react';
-
-const Counter = () => {
-    const [count, setCount] = React.useState(0);
-    useEffect (() => {
-        return (
-            <h1>Teste</h1>
-        );
-    }, [count])
-
-    const handleIncrement = () => {
-        setCount(count + 1);
-    }
-    
-    const handleDecrement = () => {
-        setCount(count - 1);
-    }
-    return(
-        <div>
-            <p>The counter is {count}</p>
-            <button onClick={handleIncrement}>Increment</button>
-            <button onClick={handleDecrement}>Decrement</button>
-        </div>
-    );
-};
-
->>>>>>> 4cb8cec9f484258094e7d3c105c11ce81dc46952
 export default Counter;
